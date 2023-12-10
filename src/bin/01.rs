ad8e45e00c6f5c6fcbec7016f212d75ec4ff6d7b
Glenn Bitar <glennbitar@gmail.com>
2023-12-10 21:28:38 +0100

    Winnow it up

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,16 +1,19 @@
-use aoc_2023::MY_COOKIE;
+use aoc_2023::{match_and_move_1, MY_COOKIE};
 use aoc_cache::get;
-use nom::branch::alt;
-use nom::bytes::complete::tag;
-use nom::IResult;
+use winnow::combinator::repeat_till0;
+use winnow::stream::Accumulate;
+use winnow::token::any;
+use winnow::{
+    combinator::{repeat, Alt},
+    token::one_of,
+    PResult, Parser,
+};
 
 fn part_one(input: &str) {
-
-    let mut s = String::with_capacity(2);
+    let mut buffer = String::with_capacity(2);
 
     let sum: u32 = input
         .lines()
-        .filter(|&l| !l.is_empty())
         .map(|line| {
             let first = line
                 .chars()
@@ -25,27 +28,28 @@ fn part_one(input: &str) {
                 .take(1)
                 .next()
                 .unwrap();
-            s.clear();
-            s.push(first);
-            s.push(last);
-            s.parse::<u32>().unwrap()
+            buffer.clear();
+            buffer.push(first);
+            buffer.push(last);
+            buffer.parse::<u32>().unwrap()
         })
         .sum();
-    println!("{sum}");
+    println!("part one: {sum}");
 }
 
-fn digit_word(input: &str) -> IResult<&str, &str> {
-    alt((
-        tag("one"),
-        tag("two"),
-        tag("three"),
-        tag("four"),
-        tag("five"),
-        tag("six"),
-        tag("seven"),
-        tag("eight"),
-        tag("nine"),
-    ))(input)
+fn digit_char(input: &mut &str) -> PResult<char> {
+    one_of(('0'..='9',)).parse_next(input)
+}
+
+fn digit_word(input: &mut &str) -> PResult<char> {
+    (
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    )
+        .choice(input)
+        .map(|word| {
+            digit_word_as_char(word)
+                .expect("a successful match on the preceding choice will return a char")
+        })
 }
 
 fn digit_word_as_char(word: &str) -> Option<char> {
@@ -63,22 +67,82 @@ fn digit_word_as_char(word: &str) -> Option<char> {
     }
 }
 
-fn digit_char(input: &str) -> IResult<&str, char>
-{
-    let (remaining, parsed) = take_
+fn digit(input: &mut &str) -> PResult<char> {
+    (digit_char, digit_word).choice(input)
+}
+
+struct FirstAndLast<T> {
+    first: Option<T>,
+    last: Option<T>,
+}
+
+impl<T: Copy> FirstAndLast<T> {
+    fn first(&self) -> T {
+        self.first.unwrap()
+    }
+    fn last(&self) -> T {
+        self.last.unwrap_or(self.first())
+    }
+}
+
+impl<T> Accumulate<T> for FirstAndLast<T> {
+    fn initial(_capacity: Option<usize>) -> Self {
+        Self {
+            first: None,
+            last: None,
+        }
+    }
+
+    fn accumulate(&mut self, acc: T) {
+        if self.first.is_none() {
+            self.first = Some(acc);
+        } else {
+            self.last = Some(acc);
+        }
+    }
 }
 
 fn part_two(input: &str) {
+    let mut buffer = String::with_capacity(2);
     let sum: u32 = input
         .lines()
-        .filter(|&l| !l.is_empty())
-        .map(|line| {
-            alt((digit_word_as_char, char::is_ascii_digit()))
-        }).sum()
+        .map(|mut line| {
+            let first_and_last: FirstAndLast<_> = repeat(
+                1..,
+                repeat_till0(any, match_and_move_1(digit)).map(|((), c)| c),
+            )
+            .parse_next(&mut line)
+            .unwrap();
+            first_and_last
+        })
+        .map(|digits| {
+            buffer.clear();
+            buffer.push(digits.first());
+            buffer.push(digits.last());
+            buffer.parse::<u32>().unwrap()
+        })
+        .sum();
+    println!("part two: {sum}");
 }
 
 fn main() {
     let input = get("https://adventofcode.com/2023/day/1/input", MY_COOKIE).unwrap();
     part_one(&input);
     part_two(&input);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::digit_word;
+    use winnow::Parser;
+
+    #[test]
+    fn digit_word_err() {
+        let input = &mut "";
+        assert!(digit_word.parse_next(input).is_err());
+    }
+    #[test]
+    fn digit_word_ok() {
+        assert_eq!(digit_word.parse_next(&mut "one"), Ok('1'));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
+use winnow::{
+    error::{ErrorKind, ParserError},
+    token::any,
+    PResult, Parser,
+};
+
 pub const MY_COOKIE: &str = include_str!("my.cookie");
+
+pub fn match_and_move_1<'s, O, E: ParserError<&'s str>>(
+    mut parser: impl Parser<&'s str, O, E>,
+) -> impl FnMut(&mut &'s str) -> PResult<O> {
+    move |input: &mut &'s str| match parser.parse_peek(*input) {
+        Ok((_remaining, output)) => {
+            let _res: PResult<char> = any.parse_next(input);
+            Ok(output)
+        }
+        Err(_) => Err(ParserError::from_error_kind(input, ErrorKind::Slice)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::match_and_move_1;
+    use winnow::{error::InputError, Parser};
+
+    #[test]
+    pub fn match_and_move_1_works() {
+        let input = &mut "ottootto";
+        let res = match_and_move_1::<_, InputError<_>>("otto").parse_next(input);
+        assert_eq!(res, Ok("otto"));
+        assert_eq!(*input, "ttootto");
+    }
+}
